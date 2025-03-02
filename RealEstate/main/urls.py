a41ef887e2a9a28de4c9456c6aca778d6f24b9e9
abdulrahman abdullah <abdulrahmanaid.21@gmail.com>
2025-03-02 18:08:57 +0300

    nav bar added

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 app_name = "name"
-urlpatterns = [path("",views.home_page_view,name="home_page_view")]
+urlpatterns = [path("",views.home_view,name="home_view")]
