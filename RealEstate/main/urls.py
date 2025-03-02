4560eafbb5ce978fcc5d0accc5a12715c8858899
Nawaf Alahmadi <nawaf.m.alahmadi@gmail.com>
2025-03-03 01:03:24 +0300

    Update -  Not Done

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = "main"
 urlpatterns = [
   path("", views.home_view, name ="home"),
+  path("properties/", views.properties_view, name ="properties"),
+  path("contact/", views.contact_view, name ="contact"),
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -3,6 +3,19 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 # Home Page
-
 def home_view(request:HttpRequest):
   return render(request, "main/index.html")
+
+# properties 
+def properties_view(request:HttpRequest):
+  properties = [
+    {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+    {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+    {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+    {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},
+]
+  return render(request, "main/properties.html", context={"properties":properties})
+
+# contact_view 
+def contact_view(request:HttpRequest):
+  return render(request, "main/contact.html")
