8d82f8ca938a200636fdbdf28a2be71a0d8625e4
elaf <elafabdualrahman00@gmail.com>
2025-03-03 20:21:02 +0300

    added light and dark modes

--- RealEstate/home/urls.py
+++ RealEstate/home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home_dark/', views.home_dark, name='home_dark'),
     path('contact/', views.contact, name='contact'),
+    path('properties/', views.properties, name='properties'),
 ]
--- RealEstate/home/views.py
+++ RealEstate/home/views.py
@@ -9,3 +9,12 @@ def home_dark(request):
 
 def contact(request):
     return render(request, 'home/contact.html')
+
+def properties(request):
+    properties = [
+        {"title": "Villa Modern in Malqa", "image": "villa1.jpg"},
+        {"title": "Great home for you in Rimal", "image": "villa2.jpg"},
+        {"title": "Villa with 8 bedrooms in Swedey", "image": "villa3.jpg"},
+        {"title": "Amazing Villa in Hitten", "image": "villa4.jpg"},
+    ]
+    return render(request, 'home/properties.html', {'properties': properties})
