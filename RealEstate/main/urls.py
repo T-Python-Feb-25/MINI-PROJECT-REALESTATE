e8a78856f245124c4ce6ecddc10405d672d2b40a
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-03-03 07:28:15 +0300

    cotntact and properties pages

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
-app_name = "name"
-urlpatterns = [path("",views.home_view,name="home_view")]
+app_name = "main"
+urlpatterns = [path("",views.home_view,name="home_view"),
+               path("properties/",views.properties_view,name="properties_view"),
+               path("contact/",views.contact_view,name="contact_view")]
