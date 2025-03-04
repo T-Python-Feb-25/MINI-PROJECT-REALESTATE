7f155edbd9c38fac633580329e2150daddc75bd1
mohamad <mo.alharbi077@gmail.com>
2025-03-04 08:03:53 +0300

    solve lab

--- RealEstate/main/urls.py
+++ RealEstate/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('',views.home_view,name='home_view')
+    path('', views.home_view, name='home_view'),
+    path('properties/', views.properties_view, name='properties_view'),
+    path('contact/', views.contact_view, name='contact_view'),
+    path('toggle-theme/', views.theme_mode_view, name='theme_mode_view'),
 ]
--- RealEstate/main/views.py
+++ RealEstate/main/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse   
+from django.shortcuts import redirect
 
 def home_view(request):
-    return render(request,'home.html')
+
+    return render(request, 'home.html')
+
+def properties_view(request):
+    context = {
+
+        "title1" : "Villa Modern in Malqa", "image1" : "villa1.jpg",
+        "title2" : "Great home for you in Rimal", "image2" : "villa2.jpg",
+        "title3" : "Villa with 8 bedrooms in Swedey", "image3" : "villa3.jpg",
+        "title4" : "Amazing Villa in Hitten", "image4" : "villa4.jpg",
+    }
+    return render(request, 'properties.html',context=context)
+
+def contact_view(request):
+
+    return render(request, 'contact.html')
+
+def theme_mode_view(request):
+
+    referer = request.META.get('HTTP_REFERER', '/')
+    response = redirect(referer)
+
+    current_mode = request.COOKIES.get('dark_mode')
+    
+    if current_mode == 'true':
+        response.set_cookie('dark_mode', 'false')
+    else:
+        response.set_cookie('dark_mode', 'true')
+    
+    return response
